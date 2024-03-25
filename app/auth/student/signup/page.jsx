996feb793636144fd683a79ca6a3bfcf771cb282
joeplevin <71765364+joeplevin@users.noreply.github.com>
2024-03-25 20:06:28 +0000

    fucking commit you horrible bastard

---
+++ app/auth/student/signup/page.jsx
@@ -0,0 +1,18 @@
+import SignupForm from "@/app/components/SignupForm";
+import { Image } from "@nextui-org/react";
+import Link from "next/link";
+
+const SignupPage = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 place-items-center items-center gap-3">
+      <div className="md:col-span-2 flex justify-center items-center">
+        <p className="text-center p-2 text-white">Already Signed up?</p>
+        <Link href={"/auth/signin"}>Sign In</Link>
+      </div>
+      <SignupForm role="student" />
+      <Image src="/login.png" alt="login" width={500} height={500} />
+    </div>
+  );
+};
+
+export default SignupPage;
